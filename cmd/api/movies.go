9c4bce0b05dcf4afcdc678e8c478122fbb517434
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-10-04 12:12:32 -0500

    Add validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/kensonjohnson/greenlight/internal/data"
+	"github.com/kensonjohnson/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +21,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 	}
 
 	fmt.Fprintf(w, "%+v\n", input)
